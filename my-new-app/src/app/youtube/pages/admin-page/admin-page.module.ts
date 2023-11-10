e4a90a84a8cc5901bc8961ef0ed9385847989acb
Anastasiya Andronova <anastasiya5.88@mail.ru>
2023-11-10 22:06:11 +0300

    feat: add routing to admin page

---
+++ my-new-app/src/app/youtube/pages/admin-page/admin-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminPageComponent } from './admin-page.component';
+
+
+
+@NgModule({
+  declarations: [AdminPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminPageModule { }
