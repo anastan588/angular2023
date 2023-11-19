0e70ec5bd95ad31584129995de875d962bfff9b8
Anastasiya Andronova <anastasiya5.88@mail.ru>
2023-11-19 23:35:59 +0300

    refactor: improve routing

--- my-new-app/src/app/shared/shared.module.ts
+++ my-new-app/src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { InputComponent } from './components/input/input.component';
 import { FilterPipe } from './pipes/filter.pipe';
 import { HeaderModule } from './components/header.module';
 import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundPageModule } from './pages/not-found/not-found-page.module';
 
 @NgModule({
   declarations: [],
@@ -22,6 +23,7 @@ import { FooterComponent } from './components/footer/footer.component';
     InputComponent,
     ButtonModule,
     HeaderModule,
+    NotFoundPageModule
   ],
 })
 export class SharedModule {}
