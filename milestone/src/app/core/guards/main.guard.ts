8401cc7d4467795a3cb009e2d6035fc52f3d4a3b
Anastasiya Andronova <anastasiya5.88@mail.ru>
2023-12-09 17:38:28 +0300

    feat: add guards for signin, signup pages, default pages, notfound email mistake

--- milestone/src/app/core/guards/main.guard.ts
+++ milestone/src/app/core/guards/main.guard.ts
@@ -2,13 +2,13 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const mainGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const authService = new AuthService(router);
   const canActivate = authService.getLocalStorageData();
   console.log(canActivate);
   if (canActivate === false) {
-    return true;
+    return false;
   }
-  return false;
+  return true;
 };
